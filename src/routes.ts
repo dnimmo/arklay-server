de526e7ce39971bf69b61a1f7b94343d30d649c8
David Nimmo <dnimmo@gmail.com>
2020-04-27 15:31:19 +0100

    serve rooms/items on request

--- src/routes.ts
+++ src/routes.ts
@@ -1,22 +1,43 @@
 import getItem from './items';
 import getRoom from './rooms';
-
+import { Item, Room } from './shared_interfaces';
 
 const version = 
   '1.0';
 
 
+const respond =
+  (value : Room | Item | undefined, res : any) => 
+      res
+          .status(value ? 200 : 404)
+          .json({ value });
+
+
 const routes = 
   function (app : { route: any }) {
-      // todo: Fix that `any` declaration!
+      // todo: Fix these `any` declarations!
       app
           .route(`/v${version}/items/:itemKey`)
-          .get(getItem);
+          .get(
+              (req : any, res : any) => {
+                  const item = 
+                    getItem(req.params.itemKey.toUpperCase());
+
+            
+                  respond(item, res);
+              });
   
       
       app
           .route(`/v${version}/rooms/:roomKey`)
-          .get(getRoom);
+          .get(
+              (req : any, res : any) => {
+                  const room = 
+                    getRoom(req.params.roomKey.toUpperCase());
+
+
+                  respond(room, res);
+              });
   };
 
 
