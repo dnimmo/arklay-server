45de38e35a9267c2b0a5f87679ceedf6bfa6f75b
David Nimmo <dnimmo@gmail.com>
2020-05-01 15:14:18 +0100

    enable CORS

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,15 @@ import { RoomKey, ItemKey } from './shared_interfaces';
 
 
 exports.itemsHandler = 
-  async (event : { itemKey : ItemKey }) => {
+  async (event : { pathParameters : { itemkey : ItemKey } }) => {
       const item = 
-        getItem(event.itemKey);
+        getItem(event.pathParameters.itemkey);
 
 
       const response = {
+          headers: { 
+              'Access-Control-Allow-Origin' : '*'
+          },
           statusCode: item ? 200 : 404,
           body: JSON.stringify(item),
       };
@@ -20,12 +23,15 @@ exports.itemsHandler =
 
 
 exports.roomsHandler = 
-  async (event : { roomKey : RoomKey }) => {
+  async (event : { pathParameters : { roomkey : RoomKey } }) => {
       const room = 
-        getRoom(event.roomKey);
+        getRoom(event.pathParameters.roomkey);
 
 
       const response = {
+          headers: { 
+              'Access-Control-Allow-Origin' : '*'
+          },
           statusCode: room ? 200 : 404,
           body: JSON.stringify(room),
       };
